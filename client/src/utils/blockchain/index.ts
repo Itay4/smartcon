33d9c3497ff4e1d4f7c9abace79efdcff4fe6385
Itay4 <31018228+Itay4@users.noreply.github.com>
2021-11-19 16:31:47 +0200

    standard

--- client/src/utils/blockchain/index.ts
+++ client/src/utils/blockchain/index.ts
@@ -1,98 +1,98 @@
-const ethers = require('ethers');
-const utils = ethers.utils;
+const ethers = require('ethers')
+const utils = ethers.utils
 
 export const createTrainer = async (userAddress: any, trainerName: any, pokemonName: any) => {
-  const pokemonTrainerContract = window.pokemonTrainerContract;
+  const pokemonTrainerContract = window.pokemonTrainerContract
 
-  trainerName = utils.formatBytes32String(trainerName);
-  pokemonName = utils.formatBytes32String(pokemonName);
+  trainerName = utils.formatBytes32String(trainerName)
+  pokemonName = utils.formatBytes32String(pokemonName)
 
-  let result = {};
+  let result = {}
 
   await pokemonTrainerContract.methods
     .createTrainer(trainerName, pokemonName)
     .send({ from: userAddress })
     .then(async (logs: { events: { NewPokemonCreated: { returnValues: any; }; }; }) => {
-      let res = logs.events.NewPokemonCreated.returnValues;
+      const res = logs.events.NewPokemonCreated.returnValues
       result = {
         name: utils.parseBytes32String(res.name),
         dna: res.dna,
-        HP: res.HP,
-      };
+        HP: res.HP
+      }
       console.log(
         'Create Trainer: ' +
           JSON.stringify(logs.events.NewPokemonCreated.returnValues)
-      );
-    });
+      )
+    })
 
-  return result;
-};
+  return result
+}
 
 export const battle = async (userAddress: any, pokemonIndex: any) => {
-  const pokemonTrainerContract = window.pokemonTrainerContract;
+  const pokemonTrainerContract = window.pokemonTrainerContract
 
-  var result;
+  let result
 
   await pokemonTrainerContract.methods
     .battleWildPokemon(pokemonIndex)
     .send({ from: userAddress })
     .then(async (logs: { events: { NewBattle: { returnValues: any; }[]; }; transactionHash: any; }) => {
-      let res = logs.events.NewBattle[0].returnValues;
+      const res = logs.events.NewBattle[0].returnValues
       result = {
         trainerPokemonName: utils.parseBytes32String(res.trainerPokemonName),
         wildPokemonName: utils.parseBytes32String(res.wildPokemonName),
         trainerPokemonHP: res.trainerPokemonHP,
         wildPokemonHP: res.wildPokemonHP,
         battleResult: res.battleResult,
-        battleHash: logs.transactionHash,
-      };
-    });
+        battleHash: logs.transactionHash
+      }
+    })
 
-  return result;
-};
+  return result
+}
 
 export const trainerPokemonCount = async (userAddress: any, contract?: undefined) => {
-  const pokemonTrainerContract = window.pokemonTrainerContract || contract;
+  const pokemonTrainerContract = window.pokemonTrainerContract || contract
 
-  let result;
+  let result
 
   await pokemonTrainerContract.methods
     .trainerPokemonCount(userAddress)
     .call({ from: userAddress })
     .then(async (logs: string) => {
-      console.log('Trainer Pokemon Count: ' + logs);
-      result = logs;
-    });
-  return result;
-};
+      console.log('Trainer Pokemon Count: ' + logs)
+      result = logs
+    })
+  return result
+}
 
 export const listTrainerPokemons = async (
   userAddress: any,
   pokemonCount: number,
   contract?: undefined
 ) => {
-  const pokemonTrainerContract = window.pokemonTrainerContract || contract;
-  var promiseArr = [];
+  const pokemonTrainerContract = window.pokemonTrainerContract || contract
+  let promiseArr = []
   for (let i = 0; i < pokemonCount; i++) {
     promiseArr.push(
       pokemonTrainerContract.methods
         .listTrainerPokemon(i)
         .call({ from: userAddress })
-    );
+    )
   }
 
-  var result: { name: any; dna: any; HP: any; pokemonIndex: number; }[] = [];
+  const result: { name: any; dna: any; HP: any; pokemonIndex: number; }[] = []
 
-  var pokemons = await Promise.all(promiseArr);
-  console.log(pokemons);
-  pokemons.map((pokemon, index) => {
+  const pokemons = await Promise.all(promiseArr)
+
+  pokemons.map((pokemon, index) => (
     result.push({
       name: utils.parseBytes32String(pokemon.name),
       dna: pokemon.dna,
       HP: pokemon.HP,
       pokemonIndex: index,
-    });
-  });
+    })
+  ))
 
-  return result;
-};
+  return result
+}
